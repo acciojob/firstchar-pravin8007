4383236703da7872c7f6cd83e9264f9738582b50
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-10-18 08:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function firstChar(text) {
-  // your code here
+function firstChar(inputString) {
+    // Remove leading and trailing spaces using trim()
+    inputString = inputString.trim();
+
+    // Check if the string is empty or contains only spaces
+    if (inputString === '') {
+        return '';
+    }
+
+    // Return the first character of the cleaned string
+    return inputString[0];
 }
 
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// Test cases
+console.log(firstChar(' Rosa Parks '));  // Output: 'R'
+console.log(firstChar(' Hello World '));  // Output: 'H'
+console.log(firstChar(' '));  // Output: ''
+console.log(firstChar(''));  // Output: ''
